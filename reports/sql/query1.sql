00ea403bb970ce9632e47401e59a560cb28211bc
Diogo Correia <me@diogotc.com>
2022-06-05 11:53:58 +0100

    Fix incorrect query because where was after group by

--- reports/sql/query1.sql
+++ reports/sql/query1.sql
@@ -1,7 +1,7 @@
 SELECT product.ean, product.descr FROM product
   NATURAL JOIN has
   NATURAL JOIN replenishment_event
-  GROUP BY product.ean, product.descr
-  HAVING SUM(replenishment_event.units) > 10
   WHERE replenishment_event.instant > '2021-12-31' AND
-    has.name = 'Barras Energéticas';
+    has.name = 'Barras Energéticas'
+  GROUP BY product.ean, product.descr
+  HAVING SUM(replenishment_event.units) > 10;
