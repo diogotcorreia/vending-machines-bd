a615e6db6b5e061ed8ee684c56a21227930522c7
Pesteves2002 <tomasesteves2002@gmail.com>
2022-06-16 00:19:55 +0100

    Add queries

--- scripts/queries.sql
+++ scripts/queries.sql
@@ -8,8 +8,22 @@ HAVING COUNT(*) >= ALL (
         FROM responsible_for
         GROUP BY tin
     );
+--2 (INCOMPLETE)
+SELECT tin
+FROM responsible_for
+    INNER JOIN simple_category ON responsible_for.cat_name = simple_category.name
+GROUP By tin
+HAVING COUNT(
+        DISTINCT cat_name
+        WHERE cat_name IN simple_category
+    ) = COUNT(simple_category);
 --3
 SELECT product.ean
 FROM product
     LEFT OUTER JOIN replenishment_event ON product.ean = replenishment_event.ean
 WHERE replenishment_event.ean IS NULL;
+--4
+Select ean
+FROM replenishment_event
+GROUP BY ean
+Having COUNT(DISTINCT tin) = 1
