b8013aef9eefdbc2f32f187247569eeac56e3060
randomicecube <diogo.marques.gaspar@tecnico.ulisboa.pt>
2022-06-17 15:45:32 +0100

    Add analytics.sql file

---
+++ scripts/analytics.sql
@@ -0,0 +1,5 @@
+-- The total amount of sold products in a given period of time, unfiltered or filtered
+-- by day of the week/county
+
+-- The total amount of sold products in a given district, unfiltered of filtered
+-- by county/category/day of the week
