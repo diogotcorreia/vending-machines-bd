ad3dbccf1b7c0104483a7d1c00e3bb80888dd71d
randomicecube <diogo.marques.gaspar@tecnico.ulisboa.pt>
2022-06-17 17:02:50 +0100

    Add OLAP queries (with placeholder conditions)

--- scripts/analytics.sql
+++ scripts/analytics.sql
@@ -1,5 +1,54 @@
--- The total amount of sold products in a given period of time, unfiltered or filtered
--- by day of the week/county
+-- The total amount of sold products in a given period of time, by day of the week,
+-- county or total
 
--- The total amount of sold products in a given district, unfiltered of filtered
--- by county/category/day of the week
+SELECT *
+FROM (
+  SELECT
+    day_week
+    count(units) AS sold_units
+  FROM sales
+  GROUP BY day_week
+) AS day_week_sold_units
+UNION (
+  SELECT
+    county
+    count(units) AS sold_units
+  FROM sales
+  GROUP BY county
+) AS county_sold_units
+UNION (
+  SELECT count(units) AS sold_units
+  FROM sales
+) AS total_sold_units
+WHERE sold_units > 0; -- placeholder: we can't do this one rn
+
+-- The total amount of sold products in a given district, by county, category,
+-- day of the week or total
+
+SELECT *
+FROM (
+  SELECT
+    county,
+    count(units) AS sold_units
+  FROM sales
+  GROUP BY county
+) AS sales_by_county
+UNION (
+  SELECT
+    name,
+    count(units) AS sold_units
+  FROM sales
+  GROUP BY name
+) AS sales_by_category
+UNION (
+  SELECT
+    day_week,
+    count(units) AS sold_units
+  FROM sales
+  GROUP BY day_week
+) AS sales_by_day_of_week
+UNION (
+  SELECT count(units) AS sold_units
+  FROM sales
+) AS total_sold_units
+WHERE district = 'Berlin'; -- placeholder
