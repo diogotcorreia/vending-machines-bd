4fd60a6cf2fdefd275fb6d7de6dbb15a8a4f6641
randomicecube <diogo.marques.gaspar@tecnico.ulisboa.pt>
2022-06-18 18:41:09 +0100

    Fix fourth query in report

--- reports/sql/report3/query4.sql
+++ reports/sql/report3/query4.sql
@@ -2,17 +2,3 @@ SELECT ean
 FROM replenishment_event
 GROUP BY ean
 HAVING COUNT(DISTINCT tin) = 1;
----list
-WITH RECURSIVE list_recurs(super_category, category) AS (
-    SELECT super_category,
-        category
-    FROM has_other
-    WHERE super_category = 'Armas'
-    UNION ALL
-    SELECT child.super_category,
-        child.category
-    FROM has_other AS child
-        INNER JOIN list_recurs AS parent ON child.super_category = parent.category
-)
-SELECT category
-FROM list_recurs AS sub_categories;
