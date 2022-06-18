09030f8690d4b678829cf87228ffcd5aa1b21721
randomicecube <diogo.marques.gaspar@tecnico.ulisboa.pt>
2022-06-18 18:55:06 +0100

    Add integrity constraints

--- reports/sql/report3/queries/query2.sql
+++ reports/sql/report3/queries/query2.sql
@@ -1,7 +1,8 @@
 SELECT retailer.name
 FROM responsible_for
     NATURAL JOIN retailer
-    INNER JOIN simple_category ON responsible_for.cat_name = simple_category.name
+    INNER JOIN simple_category
+        ON responsible_for.cat_name = simple_category.name
 GROUP BY retailer.name
 HAVING COUNT(DISTINCT cat_name) = (
     SELECT COUNT(*)
