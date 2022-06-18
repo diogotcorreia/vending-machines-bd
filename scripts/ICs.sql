95a0b6fc299098a409cf8f2569c64e8b8ae2d60f
Diogo Correia <me@diogotc.com>
2022-06-18 17:43:05 +0100

    Fix RI-1 not working correctly

--- scripts/ICs.sql
+++ scripts/ICs.sql
@@ -3,20 +3,23 @@ DROP TRIGGER IF EXISTS self_contained_category_trigger ON has_other;
 
 CREATE OR REPLACE FUNCTION self_contained_category_trigger() RETURNS trigger AS $$
 BEGIN
+  -- Tries to find NEW.super_category in the sub-categories of NEW.category.
+  -- If it does, throws exception
   IF EXISTS(
     WITH RECURSIVE list_recurs(super_category, category) AS (
       SELECT super_category,
         category
       FROM has_other
-      WHERE category = NEW.super_category
+      WHERE super_category = NEW.category
       UNION ALL
       SELECT child.super_category,
         child.category
       FROM has_other AS child
         INNER JOIN list_recurs AS parent ON child.super_category = parent.category
     )
-    SELECT category
-    FROM list_recurs AS sub_categories
+    SELECT category AS sub_categories
+    FROM list_recurs
+    WHERE category = NEW.super_category
   )
   THEN
     RAISE EXCEPTION 'Categories cannot be contained within themselves';
