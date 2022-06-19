206cd35e1a33f20728d2e47b4edc5decaa58d08e
Pesteves2002 <tomasesteves2002@gmail.com>
2022-06-19 15:16:39 +0100

    Add indexes

---
+++ scripts/indexes.sql
@@ -0,0 +1,36 @@
+---------------------
+-- First index
+---------------------
+DROP INDEX IF EXISTS idx_responsible_for_cat_name;
+CREATE INDEX idx_responsible_for_cat_name ON responsible_for USING HASH(cat_name);
+DROP INDEX IF EXISTS idx_responsible_for_cat_tin;
+CREATE INDEX idx_responsible_for_cat_tin ON responsible_for USING HASH(tin);
+DROP INDEX IF EXISTS idx_retailer_tin;
+CREATE INDEX idx_retailer_tin ON retailer USING HASH(tin);
+----------------------
+EXPLAIN ANALYSE
+SELECT DISTINCT R.name
+FROM retailer R,
+    responsible_for P
+WHERE R.tin = P.tin
+    and P.cat_name = 'Bolachas de Chocolate';
+----------------------
+-- SECOND index
+----------------------
+DROP INDEX IF EXISTS idx_product_desc;
+CREATE INDEX idx_product_desc ON product (description);
+DROP INDEX IF EXISTS idx_has_category_name_ean;
+CREATE INDEX idx_has_category_name_ean ON has_category (name, ean);
+DROP INDEX IF EXISTS idx_product_category;
+CREATE INDEX idx_product_category ON product USING HASH(category);
+DROP INDEX IF EXISTS idx_has_category_name;
+CREATE INDEX idx_has_category_name ON has_category USING HASH(name);
+----------------------
+EXPLAIN ANALYSE
+SELECT T.name,
+    count(T.ean) USING
+FROM product P,
+    has_category T
+WHERE P.category = T.name
+    and P.description like 'A%'
+GROUP BY T.name;
