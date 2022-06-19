1ee366586482f853ef83f4027426491b7dd6f360
Diogo Correia <me@diogotc.com>
2022-06-19 22:08:56 +0100

    Rewrite query 2 with except

--- scripts/queries.sql
+++ scripts/queries.sql
@@ -10,14 +10,17 @@ HAVING COUNT(DISTINCT cat_name) >= ALL (
 );
 
 --2
-SELECT retailer.name
-FROM responsible_for
+SELECT DISTINCT retailer.name FROM responsible_for RF
     NATURAL JOIN retailer
-    INNER JOIN simple_category ON responsible_for.cat_name = simple_category.name
-GROUP BY retailer.name
-HAVING COUNT(DISTINCT cat_name) = (
-    SELECT COUNT(*)
+WHERE NOT EXISTS (
+    SELECT name
     FROM simple_category
+    EXCEPT
+    SELECT name
+    FROM simple_category
+    INNER JOIN responsible_for
+        ON responsible_for.cat_name = simple_category.name
+    WHERE responsible_for.tin = RF.tin
 );
 
 --3
