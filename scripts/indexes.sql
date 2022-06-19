b87dc62bf84639a2f1c4f9511f7ae6243341ea21
randomicecube <diogo.marques.gaspar@tecnico.ulisboa.pt>
2022-06-19 18:37:21 +0100

    Fix parenthesis and caps incoherences

--- scripts/indexes.sql
+++ scripts/indexes.sql
@@ -13,14 +13,14 @@ SELECT DISTINCT R.name
 FROM retailer R,
     responsible_for P
 WHERE R.tin = P.tin
-    and P.cat_name = 'Bolachas de Chocolate';
+    AND P.cat_name = 'Bolachas de Chocolate';
 ----------------------
 -- SECOND index
 ----------------------
 DROP INDEX IF EXISTS idx_product_desc;
-CREATE INDEX idx_product_desc ON product USING HASH (description);
+CREATE INDEX idx_product_desc ON product USING HASH(description);
 DROP INDEX IF EXISTS idx_has_category_name_ean;
-CREATE INDEX idx_has_category_name_ean ON has_category USING BTREE (name, ean);
+CREATE INDEX idx_has_category_name_ean ON has_category USING BTREE(name, ean);
 DROP INDEX IF EXISTS idx_product_category;
 CREATE INDEX idx_product_category ON product USING HASH(category);
 DROP INDEX IF EXISTS idx_has_category_name;
@@ -28,9 +28,9 @@ CREATE INDEX idx_has_category_name ON has_category USING HASH(name);
 ----------------------
 EXPLAIN ANALYSE
 SELECT T.name,
-    count(T.ean) USING
+    count(T.ean)
 FROM product P,
     has_category T
 WHERE P.category = T.name
-    and P.description like 'A%'
+    AND P.description LIKE 'A%'
 GROUP BY T.name;
