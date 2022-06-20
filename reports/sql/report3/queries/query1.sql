f333fad6a8a567255f765a94a47f308f8ff42ccd
randomicecube <diogo.marques.gaspar@tecnico.ulisboa.pt>
2022-06-20 12:17:02 +0100

    Add reasoning for chosen approaches in various sections

--- reports/sql/report3/queries/query1.sql
+++ reports/sql/report3/queries/query1.sql
@@ -1,9 +1,8 @@
-SELECT retailer.name,
-    COUNT(*)
+SELECT name
 FROM responsible_for
     NATURAL JOIN retailer
-GROUP BY retailer.name
-HAVING COUNT(*) >= ALL (
+GROUP BY name
+HAVING COUNT(DISTINCT cat_name) >= ALL (
     SELECT COUNT(DISTINCT cat_name)
     FROM responsible_for
     GROUP BY tin
