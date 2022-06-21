6b9e99185e193da40bad26811ce41cb357abed27
randomicecube <diogo.marques.gaspar@tecnico.ulisboa.pt>
2022-06-21 19:26:14 +0100

    Remove NOT NULL from primary keys in entities

--- scripts/entities.sql
+++ scripts/entities.sql
@@ -17,17 +17,17 @@ DROP TABLE IF EXISTS replenishment_event CASCADE;
 ----------------------------------------
 -- Primary keys need NOT NULL ?
 CREATE TABLE category (
-    name VARCHAR(255) NOT NULL,
+    name VARCHAR(255),
     CONSTRAINT pk_category PRIMARY KEY (name) -- RI_RE1
 );
 CREATE TABLE simple_category (
-    name VARCHAR(255) NOT NULL,
+    name VARCHAR(255),
     CONSTRAINT pk_simple_category PRIMARY KEY (name),
     CONSTRAINT fk_simple_category_category FOREIGN KEY(name) REFERENCES category(name) -- RI_RE1
     -- RI_RE2
 );
 CREATE TABLE super_category (
-    name VARCHAR(255) NOT NULL,
+    name VARCHAR(255),
     CONSTRAINT pk_super_category PRIMARY KEY (name),
     CONSTRAINT fk_super_category_category FOREIGN KEY(name) REFERENCES category(name) -- RI_RE1
     -- RI_RE2
@@ -35,7 +35,7 @@ CREATE TABLE super_category (
 );
 CREATE TABLE has_other (
     super_category VARCHAR(255) NOT NULL,
-    category VARCHAR(255) NOT NULL,
+    category VARCHAR(255),
     CONSTRAINT fk_has_other_super_category FOREIGN KEY(super_category) REFERENCES super_category(name),
     CONSTRAINT pk_has_other PRIMARY KEY (category),
     CONSTRAINT fk_has_other_category FOREIGN KEY(category) REFERENCES category(name),
@@ -43,43 +43,43 @@ CREATE TABLE has_other (
     -- RI_RE5 (DONE)
 );
 CREATE TABLE product (
-    ean VARCHAR(13) NOT NULL,
+    ean VARCHAR(13),
     category VARCHAR(255) NOT NULL,
     description TEXT NOT NULL,
     CONSTRAINT pk_product PRIMARY KEY (ean),
     CONSTRAINT fk_product_category FOREIGN KEY(category) REFERENCES category(name) -- RI_RE6
 );
 CREATE TABLE has_category (
-    ean VARCHAR(13) NOT NULL,
-    name VARCHAR(255) NOT NULL,
+    ean VARCHAR(13),
+    name VARCHAR(255),
     CONSTRAINT pk_has_category PRIMARY KEY (ean, name),
     CONSTRAINT fk_has_category_ean FOREIGN KEY(ean) REFERENCES product(ean),
     CONSTRAINT fk_has_category_name FOREIGN KEY(name) REFERENCES category(name)
 );
 CREATE TABLE ivm (
-    serial_num VARCHAR(255) NOT NULL,
-    manuf VARCHAR(255) NOT NULL,
+    serial_num VARCHAR(255),
+    manuf VARCHAR(255),
     CONSTRAINT pk_ivm PRIMARY KEY (serial_num, manuf)
 );
 CREATE TABLE retail_point (
-    name VARCHAR(255) NOT NULL,
+    name VARCHAR(255),
     district VARCHAR(255) NOT NULL,
     county VARCHAR(255) NOT NULL,
     -- concelho (?)
     CONSTRAINT pk_retail_point PRIMARY KEY (name)
 );
 CREATE TABLE installed_on (
-    serial_num VARCHAR(255) NOT NULL,
-    manuf VARCHAR(255) NOT NULL,
+    serial_num VARCHAR(255),
+    manuf VARCHAR(255),
     local VARCHAR(255) NOT NULL,
     CONSTRAINT pk_installed_on PRIMARY KEY (serial_num, manuf),
     CONSTRAINT fk_installed_on_ivm FOREIGN KEY(serial_num, manuf) REFERENCES ivm(serial_num, manuf),
     CONSTRAINT fk_installed_on_retail_point FOREIGN KEY(local) REFERENCES retail_point(name)
 );
 CREATE TABLE shelf (
-    number VARCHAR(255) NOT NULL,
-    serial_num VARCHAR(255) NOT NULL,
-    manuf VARCHAR(255) NOT NULL,
+    number VARCHAR(255),
+    serial_num VARCHAR(255),
+    manuf VARCHAR(255),
     height INT NOT NULL,
     name VARCHAR(255) NOT NULL,
     CONSTRAINT pk_shelf PRIMARY KEY (number, serial_num, manuf),
@@ -87,10 +87,10 @@ CREATE TABLE shelf (
     CONSTRAINT fk_shelf_category FOREIGN KEY(name) REFERENCES category(name)
 );
 CREATE TABLE planogram(
-    ean VARCHAR(13) NOT NULL,
-    number VARCHAR(255) NOT NULL,
-    serial_num VARCHAR(255) NOT NULL,
-    manuf VARCHAR(255) NOT NULL,
+    ean VARCHAR(13),
+    number VARCHAR(255),
+    serial_num VARCHAR(255),
+    manuf VARCHAR(255),
     face VARCHAR(255) NOT NULL,
     units INT NOT NULL,
     loc VARCHAR(255) NOT NULL,
@@ -106,19 +106,19 @@ CREATE TABLE retailer(
 CREATE TABLE responsible_for(
     cat_name VARCHAR(255) NOT NULL,
     tin VARCHAR(255) NOT NULL,
-    serial_num VARCHAR(255) NOT NULL,
-    manuf VARCHAR(255) NOT NULL,
+    serial_num VARCHAR(255),
+    manuf VARCHAR(255),
     CONSTRAINT pk_responsible_for PRIMARY KEY (serial_num, manuf),
     CONSTRAINT fk_responsible_for_ivm FOREIGN KEY(serial_num, manuf) REFERENCES ivm(serial_num, manuf),
     CONSTRAINT fk_responsible_for_retailer FOREIGN KEY(tin) REFERENCES retailer(tin),
     CONSTRAINT fk_responsible_for_category FOREIGN KEY(cat_name) REFERENCES category(name)
 );
 CREATE TABLE replenishment_event(
-    ean VARCHAR(13) NOT NULL,
-    number VARCHAR(255) NOT NULL,
-    serial_num VARCHAR(255) NOT NULL,
-    manuf VARCHAR(255) NOT NULL,
-    instant TIMESTAMP NOT NULL,
+    ean VARCHAR(13),
+    number VARCHAR(255),
+    serial_num VARCHAR(255),
+    manuf VARCHAR(255),
+    instant TIMESTAMP,
     units INT NOT NULL,
     tin VARCHAR(255) NOT NULL,
     CONSTRAINT pk_replenishment_event PRIMARY KEY (ean, number, serial_num, manuf, instant),
