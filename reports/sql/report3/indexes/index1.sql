ed8e50b803a7b3692a2f98101afd9154e879b668
randomicecube <diogo.marques.gaspar@tecnico.ulisboa.pt>
2022-06-22 18:30:43 +0100

    Fix indexes for first query

--- reports/sql/report3/indexes/index1.sql
+++ reports/sql/report3/indexes/index1.sql
@@ -1,6 +1,6 @@
 DROP INDEX IF EXISTS idx_responsible_for_tin;
 CREATE INDEX idx_responsible_for_tin
-  ON responsible_for USING HASH(tin);
+  ON responsible_for USING BTREE(tin);
 DROP INDEX IF EXISTS idx_responsible_for_cat_name;
 CREATE INDEX idx_responsible_for_cat_name
   ON responsible_for USING HASH(cat_name);
--- reports/sql/report3/indexes/index2.sql
+++ reports/sql/report3/indexes/index2.sql
@@ -1,9 +1,9 @@
 DROP INDEX IF EXISTS idx_product_category;
 CREATE INDEX idx_product_category
-  ON product USING HASH(category);
+  ON product USING BTREE(category);
 DROP INDEX IF EXISTS idx_product_desc;
 CREATE INDEX idx_product_desc
   ON product USING BTREE(description);
 DROP INDEX IF EXISTS idx_has_category_name;
 CREATE INDEX idx_has_category_name
-  ON has_category USING HASH(name);
+  ON has_category USING BTREE(name);
