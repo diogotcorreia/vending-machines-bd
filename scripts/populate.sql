00dee0cbd3ad844accfac863cd556a4d5b4b3a66
randomicecube <diogo.marques.gaspar@tecnico.ulisboa.pt>
2022-06-23 13:26:31 +0100

    Revert "Allow products to be placed on shelf by subcategory of shelf's category (#37)"
    This reverts commit 74915ab82fa43eef4c9ce066563342379adf9d0d.

--- scripts/populate.sql
+++ scripts/populate.sql
@@ -529,7 +529,13 @@ VALUES ('ivm-1', 'fizz', 'Frigu'),
 -- Shelf
 ----------------------
 INSERT INTO shelf
-VALUES ('1', 'ivm-1', 'fizz', '2', 'Bolachas'),
+VALUES (
+        '1',
+        'ivm-1',
+        'fizz',
+        '2',
+        'Bolachas de Chocolate'
+    ),
     ('3', 'ivm-1', 'fizz', '2', 'Fruta'),
     ('2', 'ivm-2', 'buzz', '3', 'Fruta'),
     ('3', 'ivm-2', 'fizz', '3', 'Bolachas'),
