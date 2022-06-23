e5269ab28b8e1865536bffaea2f8fbe5edf17ae9
Diogo Correia <me@diogotc.com>
2022-06-23 14:16:00 +0100

    Replace `WITH RECURSIVE` with PL/pgSQL function (#52)

--- scripts/ICs.sql
+++ scripts/ICs.sql
@@ -7,19 +7,8 @@ BEGIN
   -- If it does, throws exception
   IF NEW.category = NEW.super_category OR
   EXISTS (
-    WITH RECURSIVE list_recurs(super_category, category) AS (
-      SELECT super_category,
-        category
-      FROM has_other
-      WHERE super_category = NEW.category
-      UNION ALL
-      SELECT child.super_category,
-        child.category
-      FROM has_other AS child
-        INNER JOIN list_recurs AS parent ON child.super_category = parent.category
-    )
-    SELECT category AS sub_categories
-    FROM list_recurs
+    SELECT *
+    FROM all_subcategories(NEW.category)
     WHERE category = NEW.super_category
   )
   THEN
